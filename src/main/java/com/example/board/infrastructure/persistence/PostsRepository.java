4c72fdc597c896c70fe5838dfad4388f4c4b5f75
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2023-11-30 22:08:41 +0900

    posts jpaRepository 설정

---
+++ src/main/java/com/example/board/infrastructure/persistence/PostsRepository.java
@@ -0,0 +1,8 @@
+package com.example.board.infrastructure.persistence;
+
+import com.example.board.domain.Posts;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+}
