bdd154a99e65a6e228a0fdba74e20ef49d12d629
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2023-11-30 22:09:07 +0900

    User JpaRepository 설정

---
+++ src/main/java/com/example/board/infrastructure/persistence/UserRepository.java
@@ -0,0 +1,7 @@
+package com.example.board.infrastructure.persistence;
+
+import com.example.board.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User , Long> {
+}
